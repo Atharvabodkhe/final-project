9689a2df3e2bdbcc2b0e8d18e02df52d5ba3787b
Atharvabodkhe <atharvarb12@gmail.com>
2025-04-19 08:33:44 +0530

    vercel error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['react-day-picker'],
+}
 
 export default nextConfig;
